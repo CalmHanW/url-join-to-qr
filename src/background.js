1bb9828496ff3fa0cc2e94ed11834d3b0d09a902
p_phanwang <wanghan.c@yuewen.com>
2022-02-17 13:48:57 +0800

    feat: url-join-to-qr

---
+++ src/background.js
@@ -0,0 +1,10 @@
+browser.runtime.onMessage.addListener(() => {
+  alert('Hello, World!');
+}
+//   function (request, sender, sendResponse) {
+//   console.log('Hello from the background')
+//   browser.tabs.executeScript({
+//     file: 'content-script.js',
+//   });
+// }
+)
